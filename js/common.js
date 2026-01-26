d1c68ce9fd3ba345a36dbee9eccec5a8021d08e0
Viktoria Zaharova <zaharova181114@gmail.com>
2026-01-27 00:15:19 +0200

    sidebar

--- js/common.js
+++ js/common.js
@@ -16,29 +16,58 @@ $(function () {
 });
 
 
+
+// parcing price
+function formatPrice(num) {
+  num = num.toString().replace(/\D/g, '');
+  return num.replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+}
+
+function parsePrice(str) {
+  return parseInt(str.replace(/\D/g, ''), 10) || 0;
+}
+
+
+$('#price-min, #price-max')
+  .on('input', function () {
+    this.value = this.value.replace(/\D/g, '');
+  })
+  .on('blur', function () {
+    $(this).val(formatPrice($(this).val()));
+  });
+
+
 // slider range
 $(function () {
-  $("#price-slider").slider({
+  const $slider = $('#price-slider');
+  const $min = $('#price-min');
+  const $max = $('#price-max');
+
+  $slider.slider({
     range: true,
-    min: 0,
-    max: 1000000,
-    values: [0, 1000000],
-    slide: function (event, ui) {
-      $("#price-min").val(ui.values[0]);
-      $("#price-max").val(ui.values[1]);
+    min: 1580,
+    max: 87400,
+    step: 1000,
+    values: [0, 65000],
+    slide: function (e, ui) {
+      $min.val(formatPrice(ui.values[0]));
+      $max.val(formatPrice(ui.values[1]));
     }
   });
 
-  $("#price-min").val($("#price-slider").slider("values", 0));
-  $("#price-max").val($("#price-slider").slider("values", 1));
+  // старт
+  $min.val(formatPrice($slider.slider('values', 0)));
+  $max.val(formatPrice($slider.slider('values', 1)));
 
-  // ввод вручную
-  $("#price-min, #price-max").on("change", function () {
-    let min = +$("#price-min").val();
-    let max = +$("#price-max").val();
+  // ручной ввод → слайдер
+  $min.add($max).on('blur', function () {
+    let min = parsePrice($min.val());
+    let max = parsePrice($max.val());
 
     if (min > max) [min, max] = [max, min];
 
-    $("#price-slider").slider("values", [min, max]);
+    $slider.slider('values', [min, max]);
+    $min.val(formatPrice(min));
+    $max.val(formatPrice(max));
   });
 });
